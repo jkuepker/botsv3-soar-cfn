26c0afb3018df36b49d2c3c66c25f5b3dcb87095
jkuepker <20614147+jkuepker@users.noreply.github.com>
2020-04-30 12:12:09 -0400

    Updates Splunk and Phantom shell scripts to use instance id as the Phantom reporting users passwords.

--- phantom-config-events.sh
+++ phantom-config-events.sh
@@ -13,11 +13,11 @@ unzip awscliv2.zip
 rm -f awscliv2.zip
 echo "Wait for Phantom REST API to become available."
 while [[ "$(curl -s -o /dev/null -w "%{http_code}" https://127.0.0.1/rest/system_info -k -u "admin:$(curl -s http://instance-data/latest/meta-data/instance-id)")" != "200" ]]; do printf "Waiting for Phantom to become available\n"; sleep 30; done
-echo "Connect Phantom REST API to Splunk for External Search"
-curl -k -u "admin:$(curl 'http://169.254.169.254/latest/meta-data/instance-id')" "https://127.0.0.1/rest/system_settings" -H 'Content-Type: application/json' -d "{\"search_settings\": {\"status\": \"Automatic\", \"elastic_search\": {\"enabled\": false}, \"splunk\": {\"local\": {\"enabled\": false}, \"remote\": {\"enabled\": true, \"rest\": {\"use_ssl\": true, \"verify_ssl\": false, \"port\": \"8089\"}, \"host\": \"${COREIP}\", \"user\": {\"search\": {\"username\": \"phantomsearchuser\", \"password\": \"phantomsearchpass\"}, \"delete\": {\"username\": \"phantomdeleteuser\", \"password\": \"phantomdeletepass\"}}, \"hec\": {\"use_ssl\": true, \"token\": \"8106a7ac-7856-4be2-a005-942c82768932\", \"verify_ssl\": false, \"port\": \"8088\"}, \"type\": \"standalone\"}}}}"
 echo "Check for VT API Key and create asset."
 source env.txt
 export $(cut -d= -f1 env.txt)
+echo "Connect Phantom REST API to Splunk for External Search"
+curl -k -u "admin:$(curl 'http://169.254.169.254/latest/meta-data/instance-id')" "https://127.0.0.1/rest/system_settings" -H 'Content-Type: application/json' -d "{\"search_settings\": {\"status\": \"Automatic\", \"elastic_search\": {\"enabled\": false}, \"splunk\": {\"local\": {\"enabled\": false}, \"remote\": {\"enabled\": true, \"rest\": {\"use_ssl\": true, \"verify_ssl\": false, \"port\": \"8089\"}, \"host\": \"${COREIP}\", \"user\": {\"search\": {\"username\": \"phantomsearchuser\", \"password\": \"${COREID}\"}, \"delete\": {\"username\": \"phantomdeleteuser\", \"password\": \"${COREID}\"}}, \"hec\": {\"use_ssl\": true, \"token\": \"8106a7ac-7856-4be2-a005-942c82768932\", \"verify_ssl\": false, \"port\": \"8088\"}, \"type\": \"standalone\"}}}}"
 if [[ ! -z "${VT_KEY}" ]]; then
   curl -k -u "admin:$(curl 'http://169.254.169.254/latest/meta-data/instance-id')" "https://127.0.0.1/rest/asset" -H 'Content-Type: application/json' -d "{\"action_whitelist\":{},\"configuration\":{\"apikey\":\"${VT_KEY}\",\"rate_limit\":true},\"disabled\": false, \"type\": \"reputation\", \"description\":\"Auto configured by CloudFormation\",\"name\":\"phantom-vt-config\",\"product_name\":\"VirusTotal\",\"product_vendor\":\"VirusTotal\", \"tags\":[]}"
 fi
